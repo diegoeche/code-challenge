dacb9086a62b407781fd04ea2c09dc204a6495ed
Diego Echeverri <diegoeche@gmail.com>
2025-04-04 18:57:30 +0200

    make it cute

--- lib/carrousel_parser.rb
+++ lib/carrousel_parser.rb
@@ -12,6 +12,22 @@ class CarrouselParser
     new(html).parse
   end
 
+  def parse
+    doc = Nokogiri::HTML.parse(rendered_html)
+
+    results = doc.css("a").select do |a|
+      self.matches_structure?(a)
+    end.map do |a|
+      extract_content(a)
+    end
+
+    {
+      'artworks' => results
+    }
+  end
+
+  private
+
   def rendered_html
     browser = Ferrum::Browser.new
     encoded = Base64.strict_encode64(html)
@@ -20,32 +36,27 @@ class CarrouselParser
     browser.body
   end
 
-  def parse
-    doc = Nokogiri::HTML.parse(rendered_html)
+  def matches_structure?(anchor)
+    children = anchor.element_children
 
-    results = doc.css("a").select do |a|
-      children = a.element_children
+    img = children[0]&.name == "img"
+    outer_div = children[1]&.name == "div"
+    inner_divs = children[1]&.element_children&.length == 2
 
-      img = children[0]&.name == "img"
-      outer_div = children[1]&.name == "div"
-      inner_divs = children[1]&.element_children&.length == 2
+    img && outer_div && inner_divs
+  end
 
-      img && outer_div && inner_divs
-    end.map do |a|
-      img = a.element_children[0]
-      img_url = img["data-iurl"] || img["data-src"] || img["src"]
+  def extract_content(anchor)
+    img = anchor.element_children[0]
+    img_url = img["data-iurl"] || img["data-src"] || img["src"]
 
-      title, extensions = a.element_children[1].element_children.map(&:text)
-      {
-        'name' => title,
-        'extensions' => [extensions],
-        'link' => "https://www.google.com#{a["href"]}",
-        'image' => img_url
-      }
-    end
+    title, extensions = anchor.element_children[1].element_children.map(&:text)
 
     {
-      'artworks' => results
+      'name' => title,
+      'extensions' => [extensions],
+      'link' => "https://www.google.com#{anchor["href"]}",
+      'image' => img_url
     }
   end
 end
