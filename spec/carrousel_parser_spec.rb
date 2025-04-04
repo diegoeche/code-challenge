64554e05d153d6502fb8f38107addc1250b119e2
Diego Echeverri <diegoeche@gmail.com>
2025-04-04 18:33:08 +0200

    don't re-run output per test

--- spec/carrousel_parser_spec.rb
+++ spec/carrousel_parser_spec.rb
@@ -1,27 +1,24 @@
-# spec/html_parser_spec.rb
 require "spec_helper"
 require_relative "../lib/carrousel_parser"
 require 'json'
 
 RSpec.describe CarrouselParser do
   context  "with the van-gogh paintings example" do
-    let(:html) {
-      File.read("./files/van-gogh-paintings.html")
-    }
+    before(:context) do
+      html = File.read("./files/van-gogh-paintings.html")
+      @result = CarrouselParser.parse(html)
+    end
 
     it "parses the HTML and returns the right amount of elements" do
-      result = CarrouselParser.parse(html)
-
-      expect(result['artworks'].length).to eq(47)
+      expect(@result['artworks'].length).to eq(47)
     end
 
     ["name", "extensions", "link", "image"].each do |attribute|
       it "matches the #{attribute} what was provided as expected output" do
         expected_output_string = File.read("./files/expected-array.json")
         expected_parsed_output = JSON.parse(expected_output_string)
-        
-        result = CarrouselParser.parse(html)
-        result_attribute = result["artworks"][0][attribute]
+
+        result_attribute = @result["artworks"][0][attribute]
         expected_attribute = expected_parsed_output["artworks"][0][attribute]
 
         expect(result_attribute).to eq(expected_attribute)
@@ -29,20 +26,19 @@ RSpec.describe CarrouselParser do
     end
   end
 
-  context  "with the manet paintings example" do
-    let(:html) {
-      File.read("./files/manet-paintings.html")
-    }
+  context "with the manet paintings example" do
+    before(:context) do
+      html = File.read("./files/manet-paintings.html")
+      @result = CarrouselParser.parse(html)
+    end
 
     it "parses the HTML and returns the right amount of elements" do
-      result = CarrouselParser.parse(html)
-
-      expect(result['artworks'].length).to eq(50)
+      expect(@result['artworks'].length).to eq(50)
     end
 
     ["name", "extensions", "link", "image"].each do |attribute|
-      it "matches the #{attribute} that was provided as expected output" do
-        expected_output = {
+      let(:expected_output) {
+        {
           "artworks" => [
             "name" => "Olympia",
             "extensions" => ["1863"],
@@ -50,11 +46,10 @@ RSpec.describe CarrouselParser do
             "image" => String
           ]
         }
+      }
 
-        result = CarrouselParser.parse(html)
-
-        File.write("manet-result.txt", result)
-        result_attribute = result["artworks"][0][attribute]
+      it "matches the #{attribute} of the first image to the " do
+        result_attribute = @result["artworks"][0][attribute]
         expected_attribute = expected_output["artworks"][0][attribute]
 
         expect(result_attribute).to match(expected_attribute)
