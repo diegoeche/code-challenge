082b5477970d04f5e4b90e442fce48095bbe5017
Diego Echeverri <diegoeche@gmail.com>
2025-04-04 18:22:46 +0200

    Add another example of a search with the carrousel

--- spec/carrousel_parser_spec.rb
+++ spec/carrousel_parser_spec.rb
@@ -4,7 +4,7 @@ require_relative "../lib/carrousel_parser"
 require 'json'
 
 RSpec.describe CarrouselParser do
-  context do
+  context  "with the van-gogh paintings example" do
     let(:html) {
       File.read("./files/van-gogh-paintings.html")
     }
@@ -25,6 +25,39 @@ RSpec.describe CarrouselParser do
         expected_attribute = expected_parsed_output["artworks"][0][attribute]
 
         expect(result_attribute).to eq(expected_attribute)
+      end
+    end
+  end
+
+  context  "with the manet paintings example" do
+    let(:html) {
+      File.read("./files/manet-paintings.html")
+    }
+
+    it "parses the HTML and returns the right amount of elements" do
+      result = CarrouselParser.parse(html)
+
+      expect(result['artworks'].length).to eq(50)
+    end
+
+    ["name", "extensions", "link", "image"].each do |attribute|
+      it "matches the #{attribute} that was provided as expected output" do
+        expected_output = {
+          "artworks" => [
+            "name" => "Olympia",
+            "extensions" => ["1863"],
+            "link" => String,
+            "image" => String
+          ]
+        }
+
+        result = CarrouselParser.parse(html)
+
+        File.write("manet-result.txt", result)
+        result_attribute = result["artworks"][0][attribute]
+        expected_attribute = expected_output["artworks"][0][attribute]
+
+        expect(result_attribute).to match(expected_attribute)
       end
     end
   end
